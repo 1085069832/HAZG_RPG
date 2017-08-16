93d94bc35539bbd2b9b762fab38bdc81a4037fec
1085069832 <1085069832@qq.com>
2017-08-16 21:47:39 +0800

    status脚本

---
+++ Assets/Scripts/UI/Status.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Status : MonoBehaviour
+{
+    [SerializeField] Text attack;
+    [SerializeField] Button attackPlusButton;
+    [SerializeField] Text def;
+    [SerializeField] Button defPlusButton;
+    [SerializeField] Text speed;
+    [SerializeField] Button speedPlusButton;
+    [SerializeField] Text pointRemainText;
+    [SerializeField] Text summaryText;
+
+
+}
