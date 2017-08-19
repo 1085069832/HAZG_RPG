3e7be13bfd0153f0cfaee4b4726d3a0ec62ba540
1085069832 <1085069832@qq.com>
2017-08-20 00:11:30 +0800

    实现角色状态面板

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -7,19 +7,19 @@ using UnityEngine;
 public class PlayerStatus : MonoBehaviour
 {
     public static PlayerStatus Instance;
-    public int grade = 1;
-    public int hp = 100;
-    public int mp = 100;
+    private int grade = 1;
+    private int hp = 100;
+    private int mp = 100;
     [SerializeField] int coin = 200;//金币
 
-    public int attack = 20;
-    public int attack_plus = 0;
-    public int def = 20;
-    public int def_plus = 0;
-    public int speed = 20;
-    public int speed_plus = 0;
-
-    public int point_remain = 0;//剩余点数
+    private int attack = 20;
+    private int attack_plus = 0;
+    private int def = 20;
+    private int def_plus = 0;
+    private int speed = 20;
+    private int speed_plus = 0;
+    [SerializeField]
+    int point_remain = 0;//剩余点数
 
     private void Awake()
     {
@@ -36,6 +36,136 @@ public class PlayerStatus : MonoBehaviour
         set
         {
             coin = value;
+        }
+    }
+
+    public int Grade
+    {
+        get
+        {
+            return grade;
+        }
+
+        set
+        {
+            grade = value;
+        }
+    }
+
+    public int Hp
+    {
+        get
+        {
+            return hp;
+        }
+
+        set
+        {
+            hp = value;
+        }
+    }
+
+    public int Mp
+    {
+        get
+        {
+            return mp;
+        }
+
+        set
+        {
+            mp = value;
+        }
+    }
+
+    public int Attack
+    {
+        get
+        {
+            return attack;
+        }
+
+        set
+        {
+            attack = value;
+        }
+    }
+
+    public int Attack_plus
+    {
+        get
+        {
+            return attack_plus;
+        }
+
+        set
+        {
+            attack_plus = value;
+        }
+    }
+
+    public int Def
+    {
+        get
+        {
+            return def;
+        }
+
+        set
+        {
+            def = value;
+        }
+    }
+
+    public int Def_plus
+    {
+        get
+        {
+            return def_plus;
+        }
+
+        set
+        {
+            def_plus = value;
+        }
+    }
+
+    public int Speed
+    {
+        get
+        {
+            return speed;
+        }
+
+        set
+        {
+            speed = value;
+        }
+    }
+
+    public int Speed_plus
+    {
+        get
+        {
+            return speed_plus;
+        }
+
+        set
+        {
+            speed_plus = value;
+        }
+    }
+
+    public int Point_remain
+    {
+        get
+        {
+            return point_remain;
+        }
+
+        set
+        {
+            point_remain = value;
         }
     }
 }
--- Assets/Scripts/UI/Status.cs
+++ Assets/Scripts/UI/Status.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,6 +14,87 @@ public class Status : MonoBehaviour
     [SerializeField] Button speedPlusButton;
     [SerializeField] Text pointRemainText;
     [SerializeField] Text summaryText;
+    [SerializeField] Button close;
+    PlayerStatus playerStatus;
+    ShowUIAnim showUIAnim;
+    int attackPlus;
+    int defPlus;
+    int speedPlus;
 
+    void Start()
+    {
+        playerStatus = GameObject.FindGameObjectWithTag(MyConstants.PLAYER).GetComponent<PlayerStatus>();
+        showUIAnim = GetComponent<ShowUIAnim>();
+    }
 
+    void OnEnable()
+    {
+        attackPlusButton.onClick.AddListener(AddAttackPlus);
+        defPlusButton.onClick.AddListener(AddDefPlus);
+        speedPlusButton.onClick.AddListener(AddSpeedPlus);
+        close.onClick.AddListener(OnCloseClick);
+    }
+
+    void OnDisable()
+    {
+        attackPlusButton.onClick.RemoveAllListeners();
+        defPlusButton.onClick.RemoveAllListeners();
+        speedPlusButton.onClick.RemoveAllListeners();
+        close.onClick.RemoveAllListeners();
+    }
+
+    void OnCloseClick()
+    {
+        showUIAnim.OnUIClose();
+    }
+
+    void AddSpeedPlus()
+    {
+        speedPlus++;
+        playerStatus.Speed++;
+        playerStatus.Point_remain--;
+    }
+
+    void AddDefPlus()
+    {
+        defPlus++;
+        playerStatus.Def++;
+        playerStatus.Point_remain--;
+    }
+
+    void AddAttackPlus()
+    {
+        attackPlus++;
+        playerStatus.Attack++;
+        playerStatus.Point_remain--;
+    }
+
+    private void Update()
+    {
+        if (!showUIAnim.isClose)
+        {
+            SetPlayerStatus();
+        }
+    }
+
+    void SetPlayerStatus()
+    {
+        attack.text = playerStatus.Attack - attackPlus + "(+" + attackPlus + ")";
+        def.text = playerStatus.Def - defPlus + "(+" + defPlus + ")";
+        speed.text = playerStatus.Speed - speedPlus + "(+" + speedPlus + ")";
+        pointRemainText.text = playerStatus.Point_remain + "";
+        summaryText.text = "攻击：" + playerStatus.Attack + " 防御：" + playerStatus.Def + " 速度：" + playerStatus.Speed;
+        if (playerStatus.Point_remain > 0)
+        {
+            attackPlusButton.gameObject.SetActive(true);
+            defPlusButton.gameObject.SetActive(true);
+            speedPlusButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            attackPlusButton.gameObject.SetActive(false);
+            defPlusButton.gameObject.SetActive(false);
+            speedPlusButton.gameObject.SetActive(false);
+        }
+    }
 }
