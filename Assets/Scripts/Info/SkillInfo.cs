2618e573d7286ad14239e786b8c2da102d3d4a7c
1085069832 <1085069832@qq.com>
2017-09-06 21:42:53 +0800

    技能面板

---
+++ Assets/Scripts/Info/SkillInfo.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//适用角色
+public enum ApplicableRole
+{
+    Swordman,
+    Magician
+}
+//作用类型
+public enum ApplyType
+{
+    Passive,
+    Buff,
+    SingleTarget,
+    MultiTarget
+}
+//作用属性
+public enum ApplyProperty
+{
+    Attack,
+    Def,
+    Speed,
+    AttackSpeed,
+    HP,
+    MP
+}
+//释放类型
+public enum ReleaseType
+{
+    Self,
+    Enemy,
+    Position
+}
+//技能信息
+public class SkillInfo
+{
+    public int id;
+    public string name;
+    public string icon_name;
+    public string des;
+    public ApplyType applyType;
+    public ApplyProperty applyProperty;
+    public int applyValue;
+    public int applyTime;
+    public int mp;
+    public int coldTime;
+    public ApplicableRole applicableRole;
+    public int level;
+    public ReleaseType releaseType;
+    public float distance;
+    public string efx_name;
+    public string aniname;
+    public float anitime = 0;
+}
